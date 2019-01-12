81fbdfd640738344e5f9af81b4640bef71732e77
Yuki Hirai <yukihirai0505@gmail.com>
2019-01-12 10:05:21 +0900

    Update InsertionSort

--- src/main/scala/com/example/sort/InsertionSort.scala
+++ src/main/scala/com/example/sort/InsertionSort.scala
@@ -3,6 +3,12 @@ package com.example.sort
 /**
   * Best: O(n)
   * Average, Worst: O(n**2)
+  *
+  * Shifts elements greater than value to the right
+  * Inserts value into proper location
+  *
+  * The optimal performance occurs when the array is already sorted,
+  * and arrays sorted in reverse order the worst performance for Insertion Sort.
   */
 object InsertionSort {
   def sort(numbers: List[Int], sorted: List[Int] = List.empty): List[Int] = {
@@ -28,5 +34,9 @@ object InsertionSort {
 
   def main(args: Array[String]): Unit = {
     println(sort(List(8, 4, 3, 7, 6, 5, 2, 1)))
+    println(sort(List(1, 9, 8, 4, 3, 10, 7, 6, 5, 2)))
+    println(sort(List(1, 2, 3, 4)))
+    println(sort(List(4, 3, 2, 1)))
+    println(sort(List(2, 5, 5, 3, 3, 2, 2, 1)))
   }
 }
