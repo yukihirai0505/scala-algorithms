64150bbdce7da635123d1b2a8a8553c718b6245c
Yuki Hirai <yukihirai0505@gmail.com>
2019-01-16 10:53:37 +0900

    Update SelectionSort

--- src/main/scala/com/example/sort/SelectionSort.scala
+++ src/main/scala/com/example/sort/SelectionSort.scala
@@ -1,25 +1,37 @@
 package com.example.sort
 
 /**
-  * Selection Sort http://en.wikipedia.org/wiki/Selection_sort
+  * Best: O(n**2)
+  * Average, Worst: O(n**2)
   *
-  * Created by yukihirai on 2017/04/02.
+  * It requires quadratic time even in the best case (i.e., when the array is already sorted).
+  * 
   */
 object SelectionSort {
+  def sort(numbers: List[Int], sorted: List[Int] = List.empty): List[Int] = {
+    numbers match {
+      case h :: last =>
+        def findMin(minIndexAndNum: (Int, Int), index: Int = 0): (Int, Int) = {
+          if (last.size == index) minIndexAndNum
+          else findMin(
+            if (minIndexAndNum._2 < last(index)) minIndexAndNum else (index, last(index)),
+            index + 1
+          )
+        }
 
-  def selectionSort[A <% Ordered[A]](list: List[A]): List[A] = {
-    def sort(orgList: List[A], sortedList: List[A]): List[A] = orgList match {
-      case h :: t => select(head = h, tail = t, Nil, sortedList)
-      case Nil => sortedList
+        val min = findMin((0, h))
+        // find min and if it's not head swap it
+        if (min._2 == h) sort(last, sorted :+ min._2)
+        else sort(last.updated(min._1, h), sorted :+ min._2)
+      case _ => sorted
     }
-    def select(head: A, tail: List[A], orgList: List[A], sortedList: List[A]): List[A] = {
-      tail match {
-        case h :: t =>
-          if (head > h) select(head = head, tail = t, h :: orgList, sortedList)
-          else select(h, t, head :: orgList, sortedList)
-        case Nil => sort(orgList = orgList, sortedList = head :: sortedList)
-      }
-    }
-    sort(list, Nil)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(sort(List(8, 4, 3, 7, 6, 5, 2, 1)))
+    println(sort(List(1, 9, 8, 4, 3, 10, 7, 6, 5, 2)))
+    println(sort(List(1, 2, 3, 4)))
+    println(sort(List(4, 3, 2, 1)))
+    println(sort(List(2, 5, 5, 3, 3, 2, 2, 1)))
   }
 }
--- src/test/scala/sort/SelectionSortSpec.scala
+++ src/test/scala/sort/SelectionSortSpec.scala
@@ -4,12 +4,8 @@ import com.example.sort.SelectionSort
 import org.scalatest._
 
 class SelectionSortSpec extends FlatSpec with Matchers {
-  "SelectionSort" should "selectionSort Sorted" in {
+  "SelectionSort" should "Sorted" in {
     val list = List(4, 2, 3, 5, 1)
-    SelectionSort.selectionSort[Int](list) === List(1, 2, 3, 4, 5)
-  }
-  "SelectionSort" should "selectionSort NotSorted" in {
-    val list = List(4, 2, 3, 5, 1)
-    list !== List(1, 2, 3, 4, 5)
+    SelectionSort.sort(list) === List(1, 2, 3, 4, 5)
   }
 }
