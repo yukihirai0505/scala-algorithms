b563c7a26cace5c07d6128e5eb06aa866ded5b4e
Yuki Hirai <yukihirai0505@gmail.com>
2019-01-18 15:31:01 +0900

    Update HeapSort

--- src/main/scala/com/example/sort/HeapSort.scala
+++ src/main/scala/com/example/sort/HeapSort.scala
@@ -5,18 +5,18 @@ package com.example.sort
   * Average, Worst: O(n log n)
   *
   * 1. Assume parent A[idx] is larger than or equal to either of its children
-  * 2. Left child is larger than its parent
-  * 3. Right child is larger than either its parent or left sibling.
+  * 2. Left child is lesser than its parent
+  * 3. Right child is larger than left sibling.
+  *
+  * Good movie => https://www.youtube.com/watch?v=X0ESspSiLIc
   */
 object HeapSort {
 
-  def swap(a: Array[Int], i: Int, j: Int): Unit = {
-    val t = a(i)
-    a(i) = a(j)
-    a(j) = t
+  def swap(a: List[Int], i: Int, j: Int): List[Int] = {
+    a.updated(i, a(j)).updated(j, a(i))
   }
 
-  def heapify(a: Array[Int], idx: Int, max: Int): Unit = {
+  def heapify(a: List[Int], idx: Int, max: Int): List[Int] = {
     var largest = idx
     val left = (2 * idx) + 1
     val right = 2 * idx + 2
@@ -27,33 +27,44 @@ object HeapSort {
     if (right < max && a(right) > a(largest)) {
       largest = right
     }
-    if (largest != idx) {
-      swap(a, idx, largest)
-      heapify(a, largest, max)
-    }
+    // until idx == largest swap and heapify
+    if (largest == idx) a
+    else heapify(swap(a, idx, largest), largest, max)
   }
 
-  // there are two ways to create heap structure => O(N) & O(N log N)
-  def buildHeap(a: Array[Int], size: Int): Unit = {
-    val k = size / 2
-    for (idx <- k to 0 by -1) {
-      heapify(a, idx, size)
+  def buildHeap(a: List[Int]): List[Int] = {
+    val k = a.length / 2
+
+    def loop(heap: List[Int], idx: Int): List[Int] = {
+      if (idx < 0) heap
+      else loop(heapify(heap, idx, a.length), idx - 1)
     }
+
+    loop(a, k)
   }
 
-  def heapSort(a: Array[Int]): Array[Int] = {
-    buildHeap(a, a.length)
-    for (i <- a.indices.reverse) {
-      // swap max to idx
-      swap(a, 0, i)
-      // create heap structure again
-      heapify(a, 0, i)
+  def heapSort(a: List[Int]): List[Int] = {
+    val heap = buildHeap(a)
+
+    def loop(sorted: List[Int], idx: Int): List[Int] = {
+      if (idx < 0) sorted
+      else loop(heapify(swap(sorted, 0, idx), 0, idx), idx - 1)
     }
-    a
+
+    loop(heap, heap.size - 1)
   }
 
   def main(args: Array[String]): Unit = {
-    heapSort(Array(1, 0, 2, 9, 3, 8, 4, 7, 5, 6))
-    heapSort(Array(3, 8, 4, 7, 5, 6, 10, 1, 0, 2, 9))
+    println(heapSort(List(1, 0, 2, 9, 3, 8, 4, 7, 5, 6)))
+    // -> heap: 1,0,2,9,3,8,4,7,5,6 6 and 3
+    // -> heap: 1,0,2,9,6,8,4,7,5,3 8 and 2
+    // -> heap: 1,0,8,9,6,2,4,7,5,3 9 and 0
+    // -> heap: 1,9,8,0,6,2,4,7,5,3 7 and 0
+    // -> heap: 1,9,8,7,6,2,4,0,5,3 9 and 1
+    // -> heap: 9,1,8,7,6,2,4,0,5,3 7 and 1
+    // -> heap: 9,7,8,1,6,2,4,0,5,3 5 and 1
+    // -> heap: 9,7,8,5,6,2,4,0,1,3
+    // heapSort(List(3, 8, 4, 7, 5, 6, 10, 1, 0, 2, 9))
+    // -> heap: 10,9,6,7,8,3,4,1,0,2,5
   }
 }
