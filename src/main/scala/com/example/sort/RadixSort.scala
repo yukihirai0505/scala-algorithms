0e08a8438050d020c565f25500d96e5db831ad10
Yuki Hirai <yukihirai0505@gmail.com>
2019-01-18 21:38:34 +0900

    Add RadixSort

---
+++ src/main/scala/com/example/sort/RadixSort.scala
@@ -0,0 +1,42 @@
+package com.example.sort
+
+/**
+  *
+  * Best: Ω(nk)
+  * Average, Worst: O(nk)
+  *
+  */
+object RadixSort {
+
+  def getKey(value: Int, digit: Int): Int = {
+    (value % scala.math.pow(10, digit)).toInt / scala.math.pow(10, digit - 1).toInt
+  }
+
+  def sort(a: List[Int], maxDigits: Int): List[Int] = {
+    // 1桁目から順に対象の箱にいれていくこと
+    def radixSort(sorted: List[Int], digit: Int = 1): List[Int] = {
+      if (digit > maxDigits) sorted
+      else {
+        val radix: Array[Array[Int]] = Array.fill(10) {
+          Array.empty
+        }
+        sorted.foreach(num => {
+          val key = getKey(num, digit)
+          val radixSet = radix(key)
+          radix(key) = radixSet :+ num
+        })
+        radixSort(radix.flatten.toList, digit + 1)
+      }
+    }
+
+    radixSort(a)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(
+      sort(
+        List(3221, 1, 10, 9680, 577, 9420, 7, 5622, 4793, 2030, 3138, 82, 2599, 743, 4127), 4
+      )
+    )
+  }
+}
